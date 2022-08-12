8fc944930a3c79b219292f0815703cb1cd2e7e0d
allvinnik <allvinnik@mail.ru>
2022-08-12 16:27:05 +0300

    email password

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -17,6 +18,22 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: [true, 'требуется ввести почту'],
+    unique: true,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Неправильный формат почты',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'требуется ввести пароль'],
+    select: false,
+  },
+}, {
+  versionKey: false,
 });
 
 module.exports = mongoose.model('user', userSchema);
